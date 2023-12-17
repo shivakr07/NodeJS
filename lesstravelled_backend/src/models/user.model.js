9242f9ae329da6121e38665a718d257297c4dfb0
shivakr07 <kumarshiva0707@gmail.com>
2023-12-17 17:25:17 +0530

    usermodel of less travelled backend and tokens added

--- lesstravelled_backend/src/models/user.model.js
+++ lesstravelled_backend/src/models/user.model.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
-        username : {
+        userName : {
             type : String,
             required : true,
             unique : true,
@@ -18,7 +18,7 @@ const userSchema = new Schema(
             lowercase : true,
             trim : true,
         },
-        fullname : {
+        fullName : {
             type : String,
             required : true,
             trim : true,
@@ -63,5 +63,41 @@ userSchema.methods.isPasswordCorrect = async function(password){
     // this.password is hashed password
     // it return true or false if matches or not 
 }
+
+userSchema.methods.generateAccessToken = function(){
+    Jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        //fullName -> is our payload name
+        // this.fullName -> is coming from the database
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+        
+        //todo: this is how access token generated
+    )
+}
+
+// but this is similar to access token ?
+//refresh token have less information [because it get refreshed more frequently so we just put id here]
+userSchema.methods.generateAccessToken = function(){
+    Jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+        //todo:  this is how refresh token generated
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){}
 
 export const User = mongoose.model("User", userSchema);
